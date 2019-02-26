5762d9912522b630befd7d64ffa13da45e2b06b4
denaSH <denamoean17@gmail.com>
2019-02-26 14:59:09 +0200

    make data base #6

---
+++ src/database/dbBuild.sql
@@ -0,0 +1,19 @@
+BEGIN;
+
+DROP TABLE IF EXISTS users ,images;
+
+create TABLE users(
+    id serial primary key,
+    username varchar(30),
+    name varchar(50),
+    password varchar
+);
+create TABLE images(
+    id serial primary key,
+    url text,
+    user_id INTEGER REFERENCES users(id)
+);
+insert into users (username, name, password)
+values
+    ('dena', 'dena', '123' ); 
+COMMIT;
